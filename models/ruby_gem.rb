a7ab1d023e7be11e5a774792929fd8e9dea0eb9c
Francesco Rodriguez <frodsan@me.com>
2014-01-06 21:01:56 -0500

    Extract Ohm::Model#fetch_by into a module.

--- models/ruby_gem.rb
+++ models/ruby_gem.rb
@@ -13,11 +13,4 @@ class RubyGem < Ohm::Model
   def self.recent
     all.sort_by(:updated_at, order: "ALPHA DESC", limit: [0, 25])
   end
-
-  def self.fetch_by(att, val)
-    keys = to_indices(att, val)
-    command = Ohm::Command.new(:sunionstore, *keys)
-
-    Ohm::MultiSet.new(key, self, command)
-  end
 end
