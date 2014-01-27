049da9f15629d1654d687d637a2cde6e814fad20
Francesco Rodriguez <frodsan@me.com>
2014-01-27 01:58:57 -0300

    Fix RubyGem statuses.

--- models/ruby_gem.rb
+++ models/ruby_gem.rb
@@ -1,14 +1,14 @@
 class RubyGem < Ohm::Model
   include Ohm::Timestamps
 
+  STATUSES = %w(ready not_ready unknown)
+
   attribute :name
   attribute :status
   attribute :notes
 
   index :name
   index :status
-
-  STATUSES = ["ready", "not ready", "unknown"]
 
   def self.recent
     all.sort_by(:updated_at, order: "ALPHA DESC", limit: [0, 25])
